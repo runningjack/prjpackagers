5ec3f585499462e857ff299ac4835336140a9c4d
Ahmed <amedora09@gmail.com>
2016-06-30 20:34:21 +0100

    phpunit tests directory set up

---
+++ packages/Amedora/NumbersToWords/tests/NumbersToWordsTest.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: USER
+ * Date: 6/30/16
+ * Time: 7:57 PM
+ */
+
+class NumbersToWordsTest extends PHPUnit_Framework_TestCase {
+
+}
+ 
---
+++ packages/Amedora/NumbersToWords/tests/phpunit.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: USER
+ * Date: 6/30/16
+ * Time: 8:27 PM
+ */
+require_once(__DIR__."/../vendor/autoload.php");
